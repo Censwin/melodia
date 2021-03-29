327a4dccf6b4dc26304aec2ddefd14e9ed9816c7
kyle <704334497@qq.com>
2021-03-29 15:18:39 +0800

    redux init finish

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import {GlobalStyle} from './style'
-import { IconStyle } from './assets/iconfont/iconfont';
+import { GlobalStyle } from './style'
+import { IconStyle } from './assets/iconfont/iconfont'
 
-import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
-import routes from './routes/index.js';
-import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config' //renderRoutes 读取路由配置转化为 Route 标签
+import routes from './routes/index.js'
+import { HashRouter } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      { renderRoutes (routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   )
 }
-
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, compose, applyMiddleware  } from "redux";
+import thunk from 'redux-thunk'
+import reducer from './reducer'
+/* redux
+applyMiddleware: (...)
+bindActionCreators: (...)
+combineReducers: (...)
+compose: (...)
+createStore: (...) 
+*/
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)) );
+
+export default store
+
