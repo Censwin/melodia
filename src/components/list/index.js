cbc26588680c6a2b26ce92be80d60e9aee32bfee
Censwin <704334497@qq.com>
2021-03-31 00:28:43 +0800

    update npm version && recommand list style && listened count format

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { getCounter } from '../../api/utils';
 import { ListWrapper, List, ListItem } from './style';
 
 function RecommendList(props) {
   const {list} = props;
-  console.log(list);
   return (
     <ListWrapper>
-      <h1>推荐歌单</h1>
+      <h1 className="title">推荐歌单</h1>
       <List>
         {
           list.map((item, index) => {
             return (
               <ListItem key={item.id + index}>
                 <div className="imgWrapper">
+                  <div className="decorate"></div>
                   <img src={item.picUrl + "?param=300x300"} width="100%" height="100%" alt="music"/>
                   <div className="played_total">
                     <i className="iconfont play">&#xe885;</i>
-                    <span className="count">{item.playCount}</span>
+                    <span className="count">{getCounter(item.playCount)}</span>
                   </div>
                 </div>
               </ListItem>
--- src/components/list/style.js
+++ src/components/list/style.js
@@ -1,13 +1,56 @@
 import styled from 'styled-components'
 
 export const ListWrapper = styled.div`
-
+  max-height: 100%;
+  .title {
+    line-height: 2em;
+    font-weight: bold;
+    padding-left: 1em;
+    font-size: 1em;
+  }
 `
 
 export const List = styled.div`
-
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `
 
 export const ListItem = styled.div`
-
+  position: relative;
+  width: 30%;
+  margin-bottom: 5px;
+  .imgWrapper {
+    .decorate {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 35px;
+      border-radius: 3px;
+      background: linear-gradient(hsla(0,0%,43%,.4),hsla(0,0%,100%,0));
+    }
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 5px;
+    }
+    .played_total {
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: 1px 3px;
+      font-size: 12px;
+      color: #fff;
+      .play {
+        vertical-align: middle;
+      }
+    }
+  }
+  &:last-child:nth-child(3n - 1) {
+    margin-right: calc(30% + 10% / 3)
+  }
+  &:last-child:nth-child(3n - 2) {
+    margin-right: calc(60% + 20% / 3)
+  }
 `
