03aa9658e830798d4fbc7685ca944f7e77bc29e6
Censwin <704334497@qq.com>
2021-04-12 23:58:02 +0800

    edit singer

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,12 +1,40 @@
 import React, { useState } from 'react'
 import Horizen from '../../baseUI/horizen-item'
 import { categoryTypes, alphaTypes } from '../../api/config'
-import { NavContainer } from './style'
+import { NavContainer,ListContainer,List,ListItem } from './style'
+import Scroll from './../../baseUI/scroll/index'
+
+const singerList = [1,2,3,4,5,6,7,8,9,10,11,12].map(item => {
+  return {
+    picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+    name: "隔壁老樊",
+    accountId: 277313426,
+  }
+})
+
+const renderSingerList = () => {
+  return (
+    <List>
+      {
+        singerList.map((item, index) => {
+          return (
+            <ListItem key={item.accountId + "" +index}>
+              <div className="img_wrapper">
+                <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+              </div>
+            </ListItem>
+          )
+        })
+      }
+    </List>
+  )
+}
 
 function Singers() {
   const [_categ, setCateg] = useState('')
   const [_alpha, setAlpha] = useState('')
   return (
+    <div>
     <NavContainer>
       <Horizen
         list={categoryTypes}
@@ -21,6 +49,12 @@ function Singers() {
         handleClick={(key)=>setAlpha(key)}
       ></Horizen>
     </NavContainer>
+    <ListContainer>
+      <Scroll>
+        { renderSingerList () }
+      </Scroll>
+    </ListContainer>
+    </div>
   )
 }
 
