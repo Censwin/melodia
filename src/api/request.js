00873f426954c9ea84befea2b9ef74f6bf7b32fa
kyle <704334497@qq.com>
2021-04-13 16:21:19 +0800

    commit 11

--- src/api/request.js
+++ src/api/request.js
@@ -1,8 +1,16 @@
-import { axiosInstance } from "./config";
+import { axiosInstance } from './config'
 
 export const getBannerRequest = () => {
   return axiosInstance.get('/banner')
 }
 export const getRecommendListRequest = () => {
-  return axiosInstance.get ('/personalized');
+  return axiosInstance.get('/personalized')
+}
+export const getHotSingerListRequest = (count) => {
+  return axiosInstance.get(`/top/artists?offset=${count}`)
+}
+export const getSingerListRequest = (type, area, alpha, offset) => {
+  return axiosInstance.get(
+    `/artist/list?type=${type}&area=${area}&initial=${alpha.toLowerCase()}`
+  )
 }
