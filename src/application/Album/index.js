32631dd880b07805faf9a36bcebee214e9334d44
kyle <704334497@qq.com>
2021-04-29 17:28:04 +0800

    finish: 15

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,23 +1,33 @@
-import React,{useEffect, useState} from 'react'
-import {Container} from './style'
-import { CSSTransition } from 'react-transition-group';
+import React, { useEffect, useState } from 'react'
+import { Container } from './style'
+import { CSSTransition } from 'react-transition-group'
+import Header from '../../baseUI/header'
 
 function Album(props) {
   const [showStatus, setShowStatus] = useState(true)
   useEffect(() => {
     // console.log(123123123);
   })
+  const HeaderProps = {
+    title: '歌手',
+    handleClick: () => {
+      console.log(123);
+      setShowStatus(false)
+    }
+  }
   return (
-      <CSSTransition in={showStatus}  
-      timeout={300} 
-      classNames="fly" 
-      appear={true} 
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
       unmountOnExit
-      onExited={props.history.goBack}>
-        <Container>
-          123
-        </Container>
-      </CSSTransition>
+      onExited={props.history.goBack}
+    >
+      <Container>
+        <Header {...HeaderProps} />
+      </Container>
+    </CSSTransition>
   )
 }
 
---
+++ src/baseUI/header/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import style from '../../assets/global-style'
+import PropTypes from "prop-types";
+
+const HeaderContainer = styled.div`
+  position: fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  >i {
+    font-size: 20px;
+    width: 20px;
+    margin: 0 10px;
+  }
+  >h1 {
+    font-size: ${style ["font-size-l"]};
+    font-weight: 700;
+  }
+`
+function Header(props) {
+  const {handleClick, title} = props
+  return (
+    <HeaderContainer>
+      <i className="iconfont back" onClick={() => handleClick()}>&#xe655;</i>
+      <h1>{title}</h1>
+      <i className="iconfont option">&#xe65c;</i>
+    </HeaderContainer>
+  )
+}
+Header.defaultProps = {
+  handleClick: () => {},
+  title: '推荐'
+}
+Header.propTypes = {
+  handleClick: PropTypes.func,
+  title: PropTypes.string
+}
+export default Header
