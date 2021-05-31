000419a0771a1c4192eaab7991554c9468b679b6
kyle <704334497@qq.com>
2021-05-31 18:09:39 +0800

    播放器

---
+++ src/application/Player/store/reducer.js
@@ -0,0 +1,17 @@
+import { fromJS } from 'immutable'
+import * as actionType from './constants'
+
+
+const defaultState = fromJS({
+
+})
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case value:
+    return 
+  
+    default:
+      return state
+  }
+}
