3183ab789caf28659564914324f77252faa82667
kyle <704334497@qq.com>
2021-06-02 15:46:15 +0800

    normalPlayer

--- src/application/Player/index.js
+++ src/application/Player/index.js
@@ -9,18 +9,34 @@ import {
   changePlayMode,
   changeFullScreen,
 } from './store/actionCreators'
-import MiniPlayer from './miniPlayer';
+import MiniPlayer from './miniPlayer'
+import NormalPlayer from './normalPlayer'
 
 function Player(props) {
   const currentSong = {
-    al: { picUrl: "https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg" },
-    name: "木偶人",
-    ar: [{name: "薛之谦"}]
+    al: {
+      picUrl:
+        'https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg',
+    },
+    name: '木偶人',
+    ar: [{ name: '薛之谦' }],
   }
+  const { fullScreen } = props
+  const { toggleFullScreenDispatch } = props
   return (
-  <div>
-    <MiniPlayer song={currentSong}/>
-  </div>)
+    <div>
+      <MiniPlayer
+        song={currentSong}
+        fullScreen={fullScreen}
+        toggleFullScreen={toggleFullScreenDispatch}
+      />
+      <NormalPlayer
+        song={currentSong}
+        fullScreen={fullScreen}
+        toggleFullScreen={toggleFullScreenDispatch}
+      />
+    </div>
+  )
 }
 
 // 映射 Redux 全局的 state 到组件的 props 上
--- src/application/Player/miniPlayer/index.js
+++ src/application/Player/miniPlayer/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import {getName} from '../../../api/request';
-import {MiniPlayerContainer} from './style'
+import React, {useRef } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import { getName } from '../../../api/utils'
+import { MiniPlayerContainer } from './style'
 
 function MiniPlayer(props) {
+  const { song, fullScreen } = props
+  const { toggleFullScreen } = props
+  const miniPlayerRef = useRef ();
   return (
-    <MiniPlayerContainer>
-      
-    </MiniPlayerContainer>
+    <CSSTransition
+      in={!fullScreen}
+      timeout={400}
+      classNames="mini"
+      onEnter={() => {
+        miniPlayerRef.current.style.display = 'flex'
+      }}
+      onExited={() => {
+        miniPlayerRef.current.style.display = 'none'
+      }}
+    >
+      <MiniPlayerContainer  ref={miniPlayerRef} onClick={() => toggleFullScreen(true)}>
+        <div className="icon">
+          <div className="imgWrapper">
+            <img
+              src={song.al.picUrl}
+              alt="转转转"
+              className="play"
+              width="40"
+              height="40"
+            />
+          </div>
+        </div>
+        <div className="text">
+          <h2 className="name">{song.name}</h2>
+          <p className="desc">{getName(song.ar)}</p>
+        </div>
+        <div className="control">
+          <i className="iconfont">&#xe650;</i>
+        </div>
+        <div className="control">
+          <i className="iconfont">&#xe640;</i>
+        </div>
+      </MiniPlayerContainer>
+    </CSSTransition>
   )
 }
 
